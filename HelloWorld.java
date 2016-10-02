5ffa1901deefacf3817df3f5afdf36ff9e29069e
evimberg <egle.vimberg@itcollege.ee>
2016-10-02 18:24:40 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Rauno_V on 02.10.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
