a679316ee89d7cb1820f5b480fb3fa40378cee7f
evimberg <egle.vimberg@itcollege.ee>
2016-10-02 18:25:18 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,5 @@
 package com.example.helloworld;
 
-/**
- * Created by Rauno_V on 02.10.2016.
- */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
